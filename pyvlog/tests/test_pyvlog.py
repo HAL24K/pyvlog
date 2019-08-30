50b52390b97ecc71760c65bd95d712f65da8d39a
sfblake <samuelfblake@gmail.com>
2019-08-30 20:25:16 +0200

    Added content test

--- pyvlog/tests/test_pyvlog.py
+++ pyvlog/tests/test_pyvlog.py
@@ -1,5 +1,27 @@
 from pyvlog.pyvlog import VLogParser
 from pyvlog.statuslogging import do_nothing
+import ujson
+
+
+def id_dict(obj):
+    return obj.__class__.__name__ == 'dict'
+
+
+def contains_key_rec(v_key, v_dict):
+    for curKey in v_dict:
+        if curKey == v_key or (id_dict(v_dict[curKey]) and contains_key_rec(v_key, v_dict[curKey])):
+            return True
+    return False
+
+
+def get_value_rec(v_key, v_dict):
+    for curKey in v_dict:
+        if curKey == v_key:
+            return v_dict[curKey]
+        elif id_dict(v_dict[curKey]) and get_value_rec(v_key, v_dict[curKey]):
+            return contains_key_rec(v_key, v_dict[curKey])
+    return None
+
 
 def test_parsing():
 
@@ -9,6 +31,13 @@ def test_parsing():
     messages = [m.decode("utf-8").strip() for m in messages]
 
     # Run them through the vlog parser
-    vlogger = VLogParser(do_nothing)
+    vlogger = VLogParser(do_nothing, logged_types=[])
     for m in messages:
         vlogger.parse_message(m)
+
+    # Load the expected output
+    with open("pyvlog/data/test.json", "rb") as f:
+        last_status = ujson.load(f)[0]
+
+    # Check both dictionaries are the same by comparing json strings
+    assert ujson.dumps(last_status) == ujson.dumps(vlogger.status), "Converted status does not agree with reference"
